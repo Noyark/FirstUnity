7d40797246f5cb32914f6b1983378978cbeda904
Noyark <lucaibrahim@hotmail.it>
2016-02-17 16:44:38 +0100

    First Commit

---
+++ Assets/ProvaTerreno/MoveBarrel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBarrel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity = new Vector3(1, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
